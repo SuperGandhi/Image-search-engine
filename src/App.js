9a19663a043978ead980f399020ce6c93f05d2e8
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-04 13:42:54 -0500

    call api unplash

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { Formik, Form, Field} from "formik";
+import { useState } from "react";
 import "./header.css"
 
-
 const App=()=> {
+  const [photos, setPhotos] = useState([])
+  console.log({photos});
   return (
     <div>
       <header>
         <Formik
           initialValues={{search:''}}
           onSubmit={async values =>{
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization' : 'Client-ID KhpcZpBiy_yUk1D6MeY7VgUYe6Z2YlM4BKXqBK2dA1A'
+              }
+            })
+            const data = await response.json()
             // call api unplash
-            console.log(values)
-          }}
-        
+            setPhotos(data.results)
+          }} 
         >
           <Form>
             <Field name="search"/>
